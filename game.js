8ea29a96c1a7051398a0543f72517b9615beb5c1
Douglas Collins <douglas.collins.chem@gmail.com>
2023-10-01 01:30:05 -0800

    First Real Commit Message
    This is the first real commit message to be entered into this
    repository. The game is now effectively finished, and ready to be
    implemented with HTML.
    
    [Ticket:1]

--- game.js
+++ game.js
@@ -15,8 +15,14 @@ function gameRound(playerSelection,computerSelection){
     let result
 
     if (playerSelection == computerSelection) result = ("TIE!")
-    else if (playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "rock" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "rock") result = "You Lose!"
-    else result = "You Win!"
+    else if (playerSelection == "paper" && computerSelection == "scissors" || 
+             playerSelection == "rock" && computerSelection == "paper" || 
+             playerSelection == "scissors" && computerSelection == "rock") 
+             result = "You Lose!"
+    else {
+        result = "You Win!"
+        wins++
+    }
 
     return pc + cc + result
 
@@ -24,11 +30,19 @@ function gameRound(playerSelection,computerSelection){
 }
 
 function game(){
-    let pc = prompt('Enter "paper","rock" or "scissors."');
-    pc = pc.toLowerCase();
-    while  (pc != "paper" && pc != "rock" && pc != "scissors") pc = prompt("Invalid choice. Please try again.");
-    let cc = getComputerChoice();
-    console.log(gameRound(pc,cc))
+    let rounds = 0
+    while (rounds < 5){
+        console.log("Round " + (rounds + 1) + "!")
+        let pc = prompt('Enter "paper","rock" or "scissors."');
+        pc = pc.toLowerCase();
+        while  (pc != "paper" && pc != "rock" && pc != "scissors") pc = prompt("Invalid choice. Please try again.");
+        let cc = getComputerChoice();
+        console.log(gameRound(pc,cc))
+        console.log("You have won "+wins+" games.")
+        rounds++
+    }
+    console.log("Good game!")
 }
 
+let wins = 0
 game()
