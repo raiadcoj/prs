3e53db86e833c335ac7be3f4308499430e39b8c7
Douglas Collins <douglas.collins.chem@gmail.com>
2023-09-30 21:24:28 -0800

    Added code for Paper Rock Scissors

---
+++ game.js
@@ -0,0 +1,34 @@
+function getComputerChoice(){
+
+let comproll = Math.floor(Math.random()*3);
+let compchoice;
+
+if (comproll == 0) compchoice = "scissors";
+else if (comproll == 1) compchoice = "rock";
+else compchoice = "paper";
+return compchoice;
+}
+
+function gameRound(playerSelection,computerSelection){
+    let pc = ("You chose "+playerSelection+".\n")
+    let cc = ("The computer chose "+computerSelection+".\n")
+    let result
+
+    if (playerSelection == computerSelection) result = ("TIE!")
+    else if (playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "rock" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "rock") result = "You Lose!"
+    else result = "You Win!"
+
+    return pc + cc + result
+
+
+}
+
+function game(){
+    let pc = prompt('Enter "paper","rock" or "scissors."');
+    pc = pc.toLowerCase();
+    while  (pc != "paper" && pc != "rock" && pc != "scissors") pc = prompt("Invalid choice. Please try again.");
+    let cc = getComputerChoice();
+    console.log(gameRound(pc,cc))
+}
+
+game()
